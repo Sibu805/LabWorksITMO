f7e84065c46bc2a5d0e51a4482ab6b70e271260c
Sibu805 <sibusndlovu@yandex.com>
2024-10-21 19:23:26 +0300

    testing

--- LAB1.py
+++ LAB1.py
@@ -4,36 +4,37 @@ WHITE ='\u001b[47m'
 RESET = '\u001b[0m'
 
 def drawer(color, length=1):
-    print(f"'{color}' * length + {RESET},  end=''")
+    print(color + " " *length + RESET,  end='')
 
 #s_vertical - ширина вертикальной полосы
 #h_horizontal - ширина горизонтальной полосы, высота и ширина флага
-def draw_flag(heigth, width, s_vertical, h_horizontal):
+def draw_flag(height, width, s_vertical, h_horizontal):
     s_start = width//3
     s_end = s_start + s_vertical
     h_start = height//2 - h_horizontal//2
     h_end = h_start + h_horizontal
 
-    for height in (height):
-        for width in (width):
-             if h_start <= height < h_end:
-                 drawer(BLUE, s_vertical)
-             elif s_start <= width < s_end:
-                 drawer(BLUE, h_horizontal)
+    for h in range(height):
+        for w in range(width):
+             if h_start <= h < h_end:
+                 drawer(BLUE)
+             elif s_start <= w < s_end:
+                 drawer(BLUE)
              else:
                   drawer(WHITE)
-           
 
-    draw_flag(height, width, s_vertical, h_horizontal)
-    
+
+        print()
+
        
+           
 def main():
     height = 20
     width = 30
     s_vertical = 10
     h_horizontal = 10
 
-    print(height, width, s_vertical, h_horizontal)
+    draw_flag(height, width, s_vertical, h_horizontal)
 
 if __name__ == "__main__":
     main()
