47a1a62c2154a51a8de308929fa0383c920f975e
Sibu805 <sibusndlovu@yandex.com>
2024-10-23 10:57:36 +0300

    only 1 left

--- LAB1.py
+++ LAB1.py
@@ -40,6 +40,27 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+# y = x/2 function graph
+def draw_graph():
+
+    for y in range(10,-11,-1):
+        for x in range(-10,11,1):
+            if x == 0 and y == 0:
+                print('+',end=' ')
+            elif x == 0:
+                print('|',end=' ')
+            elif y == 0:
+                print('-',end=' ')
+            elif y == x/2:
+                print('*',end=' ')
+            else:
+                print(' ',end=' ')
+        print()
+
+
+draw_graph()
+   
     
 
 
